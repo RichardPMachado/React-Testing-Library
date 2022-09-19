cd0319f92c7e8457583f887a5a3a64d73d1e0056
RichardPMachado <richardmachado2211@gmail.com>
2022-09-19 16:34:26 -0300

    Requisito 3 - completo

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,17 @@
-test('', () => {});
+import React from 'react';
+import userEvent from '@testing-library/user-event';
+// import { act } from 'react-dom/test-utils';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
+
+it('Na tela aparece No favorite pokemon found, se não tiver pokémons favoritos', () => {
+  renderWithRouter(<App />);
+
+  const favorites = screen.getByRole('link', { name: /favorite pokémons/i });
+
+  userEvent.click(favorites);
+
+  const msgNotFound = screen.getByText(/no favorite pokemon found/i);
+  expect(msgNotFound).toBeInTheDocument();
+});
